69992ba0336afd114d253e366b093870e01e969b
muskansugandhi <71906510+muskansugandhi@users.noreply.github.com>
2023-08-10 20:41:39 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-musk',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Bunnyecon,mfr';
+  title = 'wetreyt,mfr';
 }
